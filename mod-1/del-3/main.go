3dc4834bea7e5ea568ef68bf530bbbc317383683
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2022-01-26 12:06:57 +0100

    Wed Jan 26 11:58:52 AM CET 2022

---
+++ mod-1/del-3/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "fmt"
+    "github.com/sdushantha/is-105/mod-1/del-3/state"
+)
+func main(){
+    fmt.Println(state.ViewState)
+}
--- mod-1/del-3/state/state.go
+++ mod-1/del-3/state/state.go
@@ -6,9 +6,9 @@ import (
     "errors"
 )
 
-west := "HS rev korn kylling"
-boat := ""
-east := ""
+var west string = "HS rev korn kylling"
+var boat string = ""
+var east string = ""
 
 func ViewState() string {
     return fmt.Sprintf("[%s ---V \\____%s_____/ _______%s______Ø---]", west, boat, east)
@@ -21,15 +21,15 @@ func PutInBoat(item string){
     // put HS in the boat from West and East so that it can fetch the item
     // from the other side
     if strings.Contains(west, item) {
-        tmpWest = strings.ReplaceAll(west, item, "")
+        tmpWest := strings.ReplaceAll(west, item, "")
         tmpWest = strings.TrimSpace(tmpWest)
 
         if tmpWest == "rev kylling" || tmpWest == "kylling rev" {
-            return errors.New("You can't leave 'rev' and 'kylling' together alone")
+            errors.New("You can't leave 'rev' and 'kylling' together alone")
         }
 
         if len(strings.Fields(boat)) > 2 {
-            return errors.New("You cant bring more than TWO items/things on the boat")
+            errors.New("You cant bring more than TWO items/things on the boat")
         }
 
         west = tmpWest
@@ -37,21 +37,20 @@ func PutInBoat(item string){
 
 
     if strings.Contains(east, item) {
-        tmpEast = strings.ReplaceAll(east, item, "")
+        tmpEast := strings.ReplaceAll(east, item, "")
         tmpEast = strings.TrimSpace(tmpEast)
 
         if tmpEast == "rev kylling" || tmpEast == "kylling rev" {
-            return errors.New("You can't leave 'rev' and 'kylling' together alone")
+            errors.New("You can't leave 'rev' and 'kylling' together alone")
         }
 
         if len(strings.Fields(boat)) > 2 {
-            return errors.New("You cant bring more than TWO items/things on the boat")
+            errors.New("You cant bring more than TWO items/things on the boat")
         }
 
         east = tmpEast
     }
 
-    return true
 }
 
 func CrossRiverTo(place string){
@@ -62,6 +61,6 @@ func CrossRiverTo(place string){
         // All contents of boat gets put into east
         east = east + boat
     } else {
-        return errors.New(fmt.Sprintf("'%s' is an invalid place", place))
+        errors.New(fmt.Sprintf("'%s' is an invalid place", place))
     }
 }
